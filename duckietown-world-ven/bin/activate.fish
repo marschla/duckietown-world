d53916dfc1ff1310459f380750fb2e50e33ad62e
marschla <marschla@student.ethz.ch>
2021-01-09 11:11:57 +0100

    small changes

--- duckietown-world-ven/bin/activate.fish
+++ duckietown-world-ven/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/marco/dt-env-developer/src/duckietown-world/duckietown-world-ven"
+set -gx VIRTUAL_ENV "/home/marco/duckietown-world/duckietown-world-ven"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
